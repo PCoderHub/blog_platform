bdc5e617fb171dfd46c66a6f94e634cf494ef5ba
PCoderHub <poojapbalachandran@gmail.com>
2026-02-07 23:51:28 +0530

    wysiwyg editor initial setup done

--- client/src/components/AuthorHome.jsx
+++ client/src/components/AuthorHome.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MyEditor from "./RichTextEditor/MyEditor";
 
 function AuthorHome() {
-  return <div>Author Home</div>;
+  return (
+    <div className="w-1/2">
+      <MyEditor />
+    </div>
+  );
 }
 
 export default AuthorHome;
